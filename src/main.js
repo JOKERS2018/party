81aac9eda02212b4f5463c82ddca25064bc5dc9e
joker <278422159@qq.com>
2019-04-24 09:10:31 +0800

    joker

--- src/main.js
+++ src/main.js
@@ -1,52 +1,50 @@
-import babelpolyfill from 'babel-polyfill'
 import Vue from 'vue'
+
+import Cookies from 'js-cookie'
+
+import 'normalize.css/normalize.css' // a modern alternative to CSS resets
+
+import Element from 'element-ui'
+import './styles/element-variables.scss'
+
+import '@/styles/index.scss' // global css
+
 import App from './App'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-default/index.css'
-//import './assets/theme/theme-green/index.css'
-import VueRouter from 'vue-router'
-import store from './vuex/store'
-import Vuex from 'vuex'
-//import NProgress from 'nprogress'
-//import 'nprogress/nprogress.css'
-import routes from './routes'
-import Mock from './mock'
-Mock.bootstrap();
-import 'font-awesome/css/font-awesome.min.css'
+import store from './store'
+import router from './router'
 
-Vue.use(ElementUI)
-Vue.use(VueRouter)
-Vue.use(Vuex)
+import i18n from './lang' // internationalization
+import './icons' // icon
+import './permission' // permission control
+import './utils/error-log' // error log
 
-//NProgress.configure({ showSpinner: false });
+import * as filters from './filters' // global filters
 
-const router = new VueRouter({
-  routes
+/**
+ * If you don't want to use mock-server
+ * you want to use mockjs for request interception
+ * you can execute:
+ *
+ * import { mockXHR } from '../mock'
+ * mockXHR()
+ */
+
+Vue.use(Element, {
+  size: Cookies.get('size') || 'medium', // set element-ui default size
+  i18n: (key, value) => i18n.t(key, value)
 })
 
-router.beforeEach((to, from, next) => {
-  //NProgress.start();
-  if (to.path == '/login') {
-    sessionStorage.removeItem('user');
-  }
-  let user = JSON.parse(sessionStorage.getItem('user'));
-  if (!user && to.path != '/login') {
-    next({ path: '/login' })
-  } else {
-    next()
-  }
+// register global utility filters
+Object.keys(filters).forEach(key => {
+  Vue.filter(key, filters[key])
 })
 
-//router.afterEach(transition => {
-//NProgress.done();
-//});
+Vue.config.productionTip = false
 
 new Vue({
-  //el: '#app',
-  //template: '<App/>',
+  el: '#app',
   router,
   store,
-  //components: { App }
+  i18n,
   render: h => h(App)
-}).$mount('#app')
-
+})
